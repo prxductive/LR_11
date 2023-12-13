6e3780bf6f73bb30b6c467a6662b9d5af617115b
prxductive <144677265+prxductive@users.noreply.github.com>
2023-12-13 15:26:54 +0300

    Add files via upload

---
+++ Задание 5.pas
@@ -0,0 +1,14 @@
+﻿uses graphabc;
+var a, b, r, i: integer;
+begin
+  SetWindowWidth(600);
+  SetWindowHeight(500);
+  a:=60; b:=100; r:=10;
+  for i:=1 to 8 do begin
+   circle(a,b,r);
+   floodfill(a,b, clrandom);
+   a:=a+55;
+   b:=b+40;
+   r:=r+9
+   end;
+end.
